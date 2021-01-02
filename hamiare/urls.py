8bf3b17a1a6d04408e1b199386c016c424b61191
Hannah Le <61400632+hannahtoni09@users.noreply.github.com>
2021-01-02 18:17:07 +0700

    Add Dashboard with Pivot

--- hamiare/urls.py
+++ hamiare/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls')),
-    path('dashboard_with_pivot/', include('dashboard.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
